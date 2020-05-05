96ff69d975d5fa9c30b99acc03e6e80bb3864cb5
arham-anees <arhamanees10@gmail.com>
2020-05-05 10:07:50 -0700

    initialCommit

---
+++ WebChangeMonitor.Domain/cFile.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebChangeMonitor.Domain {
+    public class cFile {
+        public int Id { get; set; }
+        public string LocalName { get; set; }
+        public string LocalRelativePath { get; set; }
+        public string ServerPath { get; set; }
+        public int Length { get; set; }
+        public string EncodedName { get; set; }
+        public DateTime UploadDateTime { get; set; }
+    }
+}
